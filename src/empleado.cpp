c98dc992259c7561c32a19230bad0ef608d725d2
DESKTOP-18CC8V4\Daniel Álvarez <daniel10alra@gmail.com>
2021-09-24 11:38:18 -0600

    clase empleado.cpp

---
+++ src/empleado.cpp
@@ -0,0 +1,22 @@
+#include "empleado.h"
+#include <string>
+using namespace std;
+
+Empleado::Empleado(int ID, string nombre, string apellido, string correo, int tipoEmpleado, int ID_supervisor){
+    this->ID=ID;
+    this->nombre=nombre;
+    this->apellido=apellido;
+    this->correo=correo;
+    this->tipoEmpleado=tipoEmpleado;
+    this->ID_supervisor=ID_supervisor;
+}
+
+int Empleado::getID (){
+    return this->ID;
+}
+string Empleado::getNombre (){
+    return this->nombre;
+}
+string Empleado::getApellido (){
+    return this->apellido;
+}
